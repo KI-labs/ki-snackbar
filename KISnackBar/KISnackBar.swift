56c07b2ac6773d10780b49ddc405810c982469fb
Emre Cakirlar <emrecakirlar@yahoo.com>
2018-08-30 13:28:43 +0200

    - required init was made public.

--- KISnackBar/KISnackBar.swift
+++ KISnackBar/KISnackBar.swift
@@ -92,7 +92,7 @@ public final class KISnackBar: UIView {
 
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
